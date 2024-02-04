99b1f04a0437478b98c891e4ff765098ebe99103
Awwab <mawwabkhank2006@gmail.com>
2024-02-04 23:02:16 +0500

    created turtle object and moved it on keypress

--- player.py
+++ player.py
@@ -1,7 +1,18 @@
+from turtle import Turtle
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
-
-class Player:
-    pass
+"""
+Create a turtle player that starts at the bottom of the screen and listen for the "Up" keypress to move the turtle north.
+"""
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape('turtle')
+        self.penup()
+        self.left(90)
+        self.goto(STARTING_POSITION)
+        
+    def move(self):
+        self.goto(self.xcor(),  self.ycor() + 10)
