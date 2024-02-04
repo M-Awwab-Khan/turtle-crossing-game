85b21ae063dc2dd4dd7c3248f3acd80d04c444a0
Awwab <mawwabkhank2006@gmail.com>
2024-02-04 23:20:32 +0500

    implemented multiple cars generation

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
-car = CarManager()
 screen.listen()
 screen.onkeypress(player.move, 'Up')
 
 game_is_on = True
+i = 1
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-    car.move()
+    if i % 6 == 0:
+        car = CarManager()
+        car.move()
+    i += 1
