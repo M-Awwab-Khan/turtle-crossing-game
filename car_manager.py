dcf7f1fc665dd6a793dcfd7d1683d6f1c4a35fdb
Awwab <mawwabkhank2006@gmail.com>
2024-02-04 23:12:13 +0500

    implemented single car generation

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,15 @@
+from turtle import Turtle
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape('square')
+        self.shapesize(stretch_len=2, stretch_wid=1)
+        self.penup()
+        self.fillcolor(random.choice(COLORS))
+        self.goto(300, random.randint(-250, 250))
