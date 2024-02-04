cd805f5241821cd96642d4198425a413c09b072f
Awwab <mawwabkhank2006@gmail.com>
2024-02-05 00:13:15 +0500

    tweaked slightly

--- car_manager.py
+++ car_manager.py
@@ -2,6 +2,7 @@ from turtle import Turtle
 import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 0.1
 
 """
 Create cars that are 20px high by 40px wide that are randomly generated along the y-axis and move to the left edge of the screen. No cars should be generated in the top and bottom 50px of the screen (think of it as a safe zone for our little turtle). Hint: generate a new car only every 6th time the game loop runs. 
